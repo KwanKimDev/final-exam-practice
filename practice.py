13a7558058234c71164eaec407e9734580751f46
김관형 <kwan@gimgwancBookAir.home>
2024-05-22 00:24:14 +0200

    Exercise 7

--- practice.py
+++ practice.py
@@ -17,7 +17,7 @@ print("Result:", result)
 # Question 2
 # Make the version of the code object oriented. Thus, transform the code to have a class ListCalculator, which has two instance variables (x and y, both lists of integers) and lists division as a method. Add a constructor for this class, which sets the two lists, and update the lists_division method to use the instance variables instead of the function parameters. Commit this version of the code. Try to run it to make sure everything works
 
-
+"""
 class ListCalculator:
     def __init__(self, x, y):
         self.x = x
@@ -38,7 +38,7 @@ print("Result:", result)
 """
 # Question 3
 # Add at least two unit tests for the ListCalculator class to test the lists_division method. If you did not manage to have a solution for exercise 2, create two unit tests for the sample code in exercise 1. Keep the test code as a separate file and call it TestListCalculator.py Remember that this file should import the code under test e.g.from practice import ListCalculator Add the tests and commit this version of the code.
-
+"""
 import unittest
 from practice import ListCalculator
 
@@ -79,7 +79,7 @@ if __name__ == "__main__":
 
 # Question 4
 # Add a test where your inputs for lists_division  are: y = [3,5,7] and x = [5,-4,-1] Do not add any assertion in this test case What happens when you run the test? Use the debugger to explain the issue Create screenshots and add them to the readme file on github.com directly.
-
+"""
 import unittest
 from practice import ListCalculator
 
@@ -106,12 +106,12 @@ class ListCalculator:
 
         assert sum_x != 0, "Error: Division by zero"
         return sum_y / sum_x
-
+"""
 
 # Question 6
 # Change the code and rather than adding an assertion use try: except construct to deal with the division by zero case. Make it so that when you have a division by zero you print a message that says “infinite!” Commit the new version of the code
 
-
+"""
 class ListCalculator:
     def __init__(self, x, y):
         self.x = x
@@ -128,3 +128,64 @@ class ListCalculator:
             return
 
         return result
+"""
+
+# Question 7
+import luigi
+import pandas as pd
+
+csv = pd.read_csv("sales.csv")
+print(csv)
+
+
+class ReadCSVFile(luigi.Task):
+    """Task to read the CSV file into a Pandas DataFrame."""
+
+    def output(self):
+        return luigi.LocalTarget("sales.csv")
+
+    def run(self):
+        df = pd.read_csv("sales.csv")
+        df.to_csv(self.output().path, index=False)
+
+
+class CalculateTotalSales(luigi.Task):
+    """Task to calculate the total sales for each product."""
+
+    def requires(self):
+        return ReadCSVFile()
+
+    def output(self):
+        return luigi.LocalTarget("total_sales.csv")
+
+    def run(self):
+        df = pd.read_csv(self.input().path)
+        df["TotalSales"] = df["UnitPrice"] * df["Quantity"]
+        df.to_csv(self.output().path, index=False)
+
+
+## TODO Implement the following new task to filter sales above 1000:
+
+
+class FilterHighSalesProducts(luigi.Task):
+    """Task to filter the data for products with total sales greater than 100."""
+
+    def requires(self):
+        # TODO make it depend from CalculateTotalSales
+        return CalculateTotalSales()
+
+    def output(self):
+        # TODO make it produce an output called ‘high_sales_products.csv'
+        return luigi.LocalTarget("high_sales_products.csv")
+
+    def run(self):
+        df = pd.read_csv(self.input().path)
+        ## TODO ##
+        df = df[df["Quantity"] <= 1000]
+        df.to_csv(self.output().path, index=False)
+
+
+if __name__ == "__main__":
+    luigi.build(
+        [CalculateTotalSales(), FilterHighSalesProducts()], local_scheduler=True
+    )
